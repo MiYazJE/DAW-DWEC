da1fa0ef104085458a50021d5b7921ec9b547544
MiYazJE <ruben.saizserrano@gmail.com>
2019-10-11 00:08:07 +0200

    Insercion de decimales y parentesis agregados

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,5 +1,6 @@
 var vacio = true;
 var simbolo = false;
+var decimal = false;
 
 window.addEventListener('load', () => {
 
@@ -14,7 +15,7 @@ window.addEventListener('load', () => {
     }
     
     // Insertar eventos a los botones tipo operaciones
-    for (let boton of (operaciones)) {
+    for (let boton of operaciones) {
         aplicarEventoInsertarOperacion(boton);
         aplicarSombra(boton);
         eliminarSombra(boton);
@@ -37,23 +38,51 @@ window.addEventListener('load', () => {
                 vacio = true;
             }
             else {
-                inputText.value = inputText.value.substring(
-                    0, inputText.value.length - 1
-                );
+                // Si el ultimo caracter es un espacio eliminar 2 caracteres
+                // Esto pasa porque cuando introduzco un operando doy un espacio por estetica :)
+                if (inputText.value[inputText.value.length - 1] == ' ') {
+                    inputText.value = inputText.value.substring(
+                        0, inputText.value.length - 2
+                    );
+                }
+                else {
+                    inputText.value = inputText.value.substring(
+                        0, inputText.value.length - 1
+                    );
+                }
             }
         }
     });
 
+    // Calcular si la expresion es correcta, sino no hacer nada
     document.querySelector('.resultado').addEventListener('click', () => {
         if (!vacio) {
             let inputText = document.querySelector("input[type='text']");
             let evaluar = inputText.value;
             try {   
-                let res = eval(evaluar);
+                let res = eval(evaluar.replace('x', '*'));
                 inputText.value = res;
             } catch (error) {
-                
+                // nothing
             }
+        }
+    });
+
+    // Insertar decimales
+    document.querySelector('.decimal').addEventListener('click', () => {
+        if (!decimal) {
+            let inputText = document.querySelector("input[type='text']");
+            inputText.value = inputText.value + '.';
+            decimal = true;
+            vacio = false;
+        }
+    });
+
+    // Insertar parentesis
+    document.querySelector('.parentesis').addEventListener('click', () => {
+        if (!vacio && !decimal && !simbolo) {
+            let inputText = document.querySelector("input[type='text']");
+            inputText.value = '(' + inputText.value + ')';
         }
     });
 
@@ -61,8 +90,8 @@ window.addEventListener('load', () => {
 
 function aplicarEventoInsertarOperacion(boton) {
     boton.addEventListener('click', () => {
-        // Si hay ya un simbolo de operacion no dejamos insertar el siguiente 
-        if (!simbolo && !vacio) {
+        // Si hay ya un simbolo de operacion o hay un decimal mal escrito(0.) no dejamos insertar nada 
+        if (!simbolo && !vacio && !decimal) {
             let inputText = document.querySelector("input[type='text']");
             inputText.value = inputText.value + ' ' + boton.innerHTML.trim() + ' ';
             simbolo = true;
@@ -90,6 +119,6 @@ function aplicarEventoInsertarNumero(boton) {
             vacio = false;
         }
         inputText.value = inputText.value + boton.innerHTML.trim();
-        simbolo = false;
+        simbolo = decimal = false;
     });
 }
