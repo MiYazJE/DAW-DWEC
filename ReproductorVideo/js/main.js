a410086d17e0fcc0c4f100699f28a7cfeac8dfb6
MiYazJE <ruben.saizserrano@gmail.com>
2019-11-20 00:04:43 +0100

    Funcionalidad controles terminada

--- ReproductorVideo/js/main.js
+++ ReproductorVideo/js/main.js
@@ -1,9 +1,18 @@
 
-let video        = document.querySelector('video');
-let wrapControls = document.querySelector('.wrap-controls');
-let btnPlayPause = document.querySelector('.btn-play-pause');
+let video         = document.querySelector('video');
+let wrapControls  = document.querySelector('.wrap-controls');
+let btnPlayPause  = document.querySelector('.btn-play-pause');
+let btnForward    = document.querySelector('.btn-forward');
+let btnBackward   = document.querySelector('.btn-backward');
+let btnRefresh    = document.querySelector('.btn-refresh');
+let btnMute       = document.querySelector('.btn-mute');
+let btnVolumeUp   = document.querySelector('.btn-volume-up');
+let btnVolumeDown = document.querySelector('.btn-volume-down');
+let textTime      = document.querySelector('.time');
 
 let eventDisplayControls;
+let muted = false;
+let lastVolume;
 
 // Mostrar los controles cuando se pase el raton por encima del video
 video.addEventListener('mouseover', () => {
@@ -18,8 +27,19 @@ video.addEventListener('mouseleave', () => {
     }, 8000)
 })
 
+// EVENTOS CONTROLES
 btnPlayPause.addEventListener('click', playPause);
 video.addEventListener('click', playPause);
+btnForward.addEventListener('click', forwardVideo);
+btnBackward.addEventListener('click', backwardVideo);
+btnRefresh.addEventListener('click', refreshVideo)
+btnMute.addEventListener('click', muteVideo);
+btnVolumeUp.addEventListener('click', () => {
+    if (video.volume <= 0.9) video.volume += 0.1;
+});
+btnVolumeDown.addEventListener('click', () => {
+    if (video.volume >= 0.1) video.volume -= 0.1;
+});
 
 function playPause() {
     
@@ -39,4 +59,51 @@ function playPause() {
     let far = document.querySelector('.far'); 
     far.classList.add('animation');
     far.addEventListener('animationend', () => far.classList.remove('animation'));
+}
+
+function forwardVideo() {
+    video.currentTime += 10;
+    video.play();
+}
+
+function backwardVideo() {
+    video.currentTime -= 10;
+    video.play();
+}
+
+function refreshVideo() {
+    video.currentTime = 0;
+    video.play();
+}
+
+function muteVideo() {
+    if (muted) {
+        video.volume = lastVolume;
+    }
+    else {
+        lastVolume = video.volume;
+        video.volume = 0;
+    }
+    muted = !muted;
+}
+
+video.ontimeupdate = () => {
+    textTime.innerHTML = getParseCurrentTime(video.currentTime) + '/' + getParseCurrentTime(video.duration);
+}
+
+const getParseCurrentTime = (time) => {
+    
+    time = parseInt(time);
+
+    let secs, mins;
+    mins = parseInt((time / 60));
+    secs = parseInt(time % 60);
+    
+    secs = String(secs);
+    while (secs.length < 2) secs = '0' + secs;
+
+    mins = String(mins);
+    while (mins.lenght < 2) mins = '0' + secs;
+
+    return mins + ':' + secs;
 }
